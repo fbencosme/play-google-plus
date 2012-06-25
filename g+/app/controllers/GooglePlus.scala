5000c5cbbff781b56b7e4d8a75b50baf53fc46a3
Fausto Bencosme <fbencosme@kitsd.com>
2012-06-24 22:32:31 -0400

    using .md

--- g+/app/controllers/GooglePlus.scala
+++ g+/app/controllers/GooglePlus.scala
@@ -1,13 +1,12 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
+/**
+ * @version 1.0
+ * @author Fausto Bencosme
+ */
 object GooglePlus extends Controller {
-  
-//  def index = Action {
-//    Ok(views.html.index("Your new application is ready."))
-//  }
   
   def isInline(arg: String) = {
     "inline".equals(arg)
---
+++ samples/README.md
@@ -0,0 +1,13 @@
+# play-google-plus 
+
+Google+ and Play Framework integration.
+
+## Running the sample applications
+
+```
+$ cd ~/workspace/play-google-plus/samples
+$ play run
+```
+> The application will be available on port 9000.
+
+
